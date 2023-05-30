db164a09a540e80ac17ae34f6932666ad9572dbb
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-05-30 17:41:26 +0200

    made some progress with the login pages

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,9 +1,35 @@
 import Container from "../container"
 import SiteLogo from "../../icons/SiteLogo";
 import { Link } from "react-router-dom";
+import { useState } from 'react'
+import EyeLogoOn from "../../icons/EyeLogoOn";
+import EyeLogoOff from "../../icons/EyeLogoOff";
+
 
 
 const Login = () => {
+
+    const [inputUsername, setInputUsername] = useState("")
+    const [inputPassword, setInputPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+
+    const onClick = (event) => {
+        event.preventDefault()
+        setShowPassword(!showPassword)
+
+    }
+
+    const onChangeUsername = (event) => {
+        setInputUsername(event.target.value)
+       
+    }
+
+    const onChangePassword = (event) => {
+        setInputPassword(event.target.value)
+       
+    }
+
+
     return (
 
         <>
@@ -15,17 +41,36 @@ const Login = () => {
                         Username
                     </strong>
                 </label>
-                <input type='text' name ='username'/>
+                <input type='text' name ='username' onChange={onChangeUsername} value={inputUsername}/>
 
                 <label id ='password'>
                     <strong>
                         Password
                     </strong>
                 </label>
-                <input type='text' name ='password'/>
+
+                {showPassword 
+                ? 
+                <>
+                <input type="text" name="password" onChange={onChangePassword} value={inputPassword}/>
+                <button onClick={onClick}>
+                <EyeLogoOff />
+                </button>
+                </>
+                : 
+                <>
+                <input type='password' name ='password' onChange={onChangePassword} value={inputPassword}/>
+                <button onClick={onClick}>
+                <EyeLogoOn />
+                </button>
+                </>
+                }
+
                 <button>
                     Login
                 </button>
+
+
 
                 <div>
                     <p>Don't have an account?</p>
--- src/pages/new-account/index.js
+++ src/pages/new-account/index.js
@@ -1,11 +1,110 @@
 import Container from "../container"
+import SiteLogo from "../../icons/SiteLogo"
+import {useState} from 'react'
+import EyeLogoOff from "../../icons/EyeLogoOff"
+import EyeLogoOn from "../../icons/EyeLogoOn"
+
 
 const CreateNewAccount = () => {
+
+    const [inputUsername, setInputUsername] = useState("")
+    const [inputPassword, setInputPassword] = useState("")
+    const [inputRepeatPassword, setRepeatPassword] = useState("")
+    const [showPassword, setShowPassword] = useState(false)
+    const [showRepeat, setShowRepeat] = useState(false)
+
+    const onChangeUsername = (event) => {
+        setInputUsername(event.target.value)
+       
+    }
+
+    const onChangePassword = (event) => {
+        setInputPassword(event.target.value)
+       
+    }
+
+    const onChangeRepeat = (event) => {
+        setRepeatPassword(event.target.value)
+    }
+
+    const onClick = (event) => {
+        event.preventDefault()
+        setShowPassword(!showPassword)
+
+    }
+
+    const onClickRepeat = (event) => {
+        event.preventDefault()
+        setShowRepeat(!showRepeat)
+
+    }
+
 
     return (
         <>
             <Container>
                 <form>
+                    <SiteLogo />
+                    <label id = 'username'>
+                        <strong>
+                            Set Username
+                        </strong>
+                    </label>
+                    <input type="text" name="username" onChange={onChangeUsername} value={inputUsername}/>
+
+                    <label id ='setpassword'>
+                        <strong>
+                            Set Password
+                        </strong>
+                    </label>
+
+                    {showPassword 
+                    ? 
+                    <>
+                    <input type="text" name="setpassword" onChange= {onChangePassword} value={inputPassword}/>
+                    <button onClick={onClick}>
+                    <EyeLogoOff />
+                    </button>
+                    </>
+                    : 
+                    <>
+                    <input type='password' name ='setpassword'  onChange={onChangePassword} value={inputPassword} />
+                    <button onClick={onClick}>
+                    <EyeLogoOn />
+                    </button>
+                    </>
+                    }
+
+                    <label id ='repeatpassword'>
+                        <strong>
+                            Re-enter Password
+                        </strong>
+                    </label>
+
+                    {showRepeat
+                    ? 
+                    <>
+                    <input type="text" name="repeatpassword" onChange= {onChangeRepeat} value={inputRepeatPassword}/>
+                    <button onClick={onClickRepeat}>
+                    <EyeLogoOff />
+                    </button>
+                    </>
+                    : 
+                    <>
+                    <input type='password' name ='repeatpassword'  onChange={onChangeRepeat} value={inputRepeatPassword} />
+                    <button onClick={onClickRepeat}>
+                    <EyeLogoOn />
+                    </button>
+                    </>
+                    }
+
+
+                <button>
+                    Create an Account
+                </button>
+
+
+
 
                     
                 </form>

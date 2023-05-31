a340b7f4018316f522f90395e46f0a1dd6477ad7
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-05-31 10:51:31 +0200

    some work

--- src/pages/main-page/index.js
+++ src/pages/main-page/index.js
@@ -1,16 +1,23 @@
 import { useState } from 'react'
+import {Link} from 'react-router-dom'
 import Calendar from 'react-calendar'
 
 
 const Main = () => {
-    
+
     const [date, selectDate] = useState(new Date());
     
 
     return (
+
+        <>
         <div>
             <Calendar onChange={selectDate} value={date} />
         </div>
+
+        <Link to={'/login'}>Logout</Link>
+        
+        </>
     );
 }
 
